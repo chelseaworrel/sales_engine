0c241e4d6802835bcc04396070368bd53b0f3028
Chelsea Worrel <chelseaworrel@gmail.com>
2015-03-04 15:37:06 -0700

    fixed hound syntax errors

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -7,13 +7,13 @@ require_relative './merchant_repository'
 require_relative './transaction_repository'
 
 class SalesEngine
-attr_reader :customer_repository
-            :invoice_repository
-            :invoice_item_repository
-            :item_repository
-            :merchant_repository
-            :transaction_repository
-# 
+  attr_reader :customer_repository,
+              :invoice_repository,
+              :invoice_item_repository,
+              :item_repository,
+              :merchant_repository,
+              :transaction_repository
+#
 # def initialize
 #
 # end
@@ -21,5 +21,4 @@ attr_reader :customer_repository
 # def start_up
 #
 # end
-
 end
